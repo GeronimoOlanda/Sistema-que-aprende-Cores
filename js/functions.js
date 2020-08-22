ff3eb7472d70106fa9e22b1760fa53b5d3195139
GeronimoOlanda <51466744+GeronimoOlanda@users.noreply.github.com>
2020-08-22 13:26:01 -0300

    Inserindo o SpeechRecognation e finalizando o Projeto

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,10 @@ var engine = {
         },
         "moedas":0
 }
+
+const Audiomoeda = new Audio('../audio/moeda.mp3');
+const audioErrou = new Audio('../audio/errou.mp3');
+
 function sortearCor(){
     var IndexcorSorteada = Math.floor(Math.random()*engine.cores.length);
     var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
@@ -28,3 +32,58 @@ function aplicaCorNaCaixa(nomeDaCor){
     caixaDasCores.style.backgroundImage = "url('../img/caixa-fechada.png')";
     caixaDasCores.style.backgroundSize="100%";
 }
+
+function atualizarPontuacao(valor){
+    var pontuacao = document.getElementById('pontuacao-atual')
+
+    engine.moedas += valor;
+    if(valor < 0){
+       
+    audioErrou.play();
+    }else{
+    Audiomoeda.play();
+    }
+    pontuacao.innerText = engine.moedas;
+}
+
+aplicaCorNaCaixa(sortearCor());
+
+//speechRecognition
+var btnGravador = document.getElementById('btn-responder');
+var transcricaoAudio = "";
+var respostaCorreta = ""; 
+
+if(window.SpeechRecognition || window.webkitSpeechRecognition){
+    var speechAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var gravador = new speechAPI();
+    gravador.continuous = false;
+    gravador.lang ="en-US";
+    gravador.onstart = function(){
+        btnGravador.innerText = "Estou Ouvindo";
+        btnGravador.style.backgroundColor = "white";
+        btnGravador.style.color = "black";
+    }
+    gravador.onend = function(){
+        btnGravador.innerText = "Responder";
+        btnGravador.style.backgroundColor = "transparent";
+        btnGravador.style.color = "white";
+    }
+    gravador.onresult = function(event){
+        transcricaoAudio = event.results[0][0].transcript.toUpperCase();
+        respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
+        if(transcricaoAudio === respostaCorreta){
+            atualizarPontuacao(1)
+        }else{
+            atualizarPontuacao(-1);
+        }
+        aplicaCorNaCaixa(sortearCor());
+        console.log(transcricaoAudio)
+    }
+
+}else{
+    alert('Navegador não tem suporte')
+}
+
+btnGravador.addEventListener('click',function(){
+    gravador.start();
+})
